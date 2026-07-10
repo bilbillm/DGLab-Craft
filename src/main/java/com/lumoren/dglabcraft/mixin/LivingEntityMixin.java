5f69b1bb35946fb62516240e4ce1911d9f6349ff
bilbillm <1903242199@qq.com>
2026-07-10 11:29:28 +0800

    Limit Fabric build to exact Minecraft version

--- src/main/java/com/lumoren/dglabcraft/mixin/LivingEntityMixin.java
+++ src/main/java/com/lumoren/dglabcraft/mixin/LivingEntityMixin.java
@@ -1,7 +1,6 @@
 package com.lumoren.dglabcraft.mixin;
 
 import com.lumoren.dglabcraft.DGLabCraft;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -12,17 +11,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-    @Inject(method = "hurt", at = @At("HEAD"), require = 0)
+    @Inject(method = "hurt", at = @At("HEAD"))
     private void dglabcraft$cacheDamageSource(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        dglabcraft$cacheDamageSource(source);
-    }
-
-    @Inject(method = "method_64397", at = @At("HEAD"), require = 0, remap = false)
-    private void dglabcraft$cacheDamageSource12111(ServerLevel level, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        dglabcraft$cacheDamageSource(source);
-    }
-
-    private void dglabcraft$cacheDamageSource(DamageSource source) {
         Object self = this;
         if (self instanceof Player player) {
             DGLabCraft.DAMAGE_HANDLER.cacheDamageSource(player, source);
